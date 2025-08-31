d7b0fb9a71258abab3029ce12471f30598a6d010
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-08-31 12:09:40 +0300

    ♻️ refactor(broker): remove `NoHeartbeatMiddleware` from Redis broker configuration
    - Simplified broker initialization by eliminating unused `NoHeartbeatMiddleware`.

--- src/fuelrod_exporter/core/broker.py
+++ src/fuelrod_exporter/core/broker.py
@@ -8,14 +8,8 @@ from fuelrod_exporter.config import Config
 result_backend = RedisBackend(url=Config.RESULT_BACKEND)
 
 
-class NoHeartbeatMiddleware(Middleware):
-    def after_process_message(self, broker, message, *, result=None, exception=None):
-        pass
-
-
 broker = RedisBroker(url=Config.BROKER_URL)
 
-broker.add_middleware(NoHeartbeatMiddleware())
 broker.add_middleware(Results(backend=result_backend))
 
 dramatiq.set_broker(broker)
