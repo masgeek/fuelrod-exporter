41c9474fafe7d569f82670ff6e0a8a74f644d798
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-09-01 15:17:13 +0300

    🔧 chore(tasks): register additional task modules in `__init__.py`
    - Imported `exporter`, `cleanup`, and `system_tasks` to ensure task modules are initialized.

--- src/fuelrod_exporter/tasks/__init__.py
+++ src/fuelrod_exporter/tasks/__init__.py
@@ -1,2 +1,6 @@
 # fuelrod_exporter/tasks/__init__.py
 import fuelrod_exporter.core.broker  # ensures broker is registered
+
+import fuelrod_exporter.tasks.exporter
+import fuelrod_exporter.tasks.cleanup
+import fuelrod_exporter.tasks.system_tasks
