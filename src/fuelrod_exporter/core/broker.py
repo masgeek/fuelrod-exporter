0dea873157592e04c7553ab97b0eee200cf34f3a
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-09-01 11:52:33 +0300

    ♻️ refactor(broker): remove unnecessary blank lines in Redis broker configuration

--- src/fuelrod_exporter/core/broker.py
+++ src/fuelrod_exporter/core/broker.py
@@ -2,11 +2,10 @@ import dramatiq
 from dramatiq.brokers.redis import RedisBroker
 from dramatiq.results import Results
 from dramatiq.results.backends.redis import RedisBackend
-from dramatiq.middleware import Middleware
+
 from fuelrod_exporter.config import Config
 
 result_backend = RedisBackend(url=Config.RESULT_BACKEND)
-
 
 broker = RedisBroker(url=Config.BROKER_URL)
 
