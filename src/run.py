36fe8000d9ba24056304268797fbd52f31c4fe04
Munywele Sammy Barasa <sammy@munywele.co.ke>
2025-09-02 08:20:34 +0300

    ✨ feat(run): improve Flask app initialization with conditional imports and debug handling
    - Simplified and clarified initialization steps in script documentation.
    - Added conditional import for `watch_env` to enable auto-reloading only in debug mode.
    - Streamlined handling of debug mode and environment variables.

--- src/run.py
+++ src/run.py
@@ -1,40 +1,38 @@
 """
 This script initializes and runs a Flask web application.
 
-The script performs the following steps:
-1. Loads environment variables from a `.env` file using `python-dotenv`.
-2. Creates an instance of the Flask application using a factory method `create_app()`.
-3. Retrieves the Flask debug mode, server host, and port settings from environment variables.
-4. Runs the Flask application with the specified host, port, and debug mode.
+Steps:
+1. Loads environment variables from `.env`.
+2. Creates the Flask app using `create_app()`.
+3. If in debug mode, activates auto-reload via `watch_env`.
+4. Runs the Flask app with specified host, port, and debug mode.
 
 Environment Variables:
-- `FLASK_DEBUG`: If set to '1', the application will run in debug mode.
-- `SERVER_HOST`: Specifies the host address on which the Flask app will run. Defaults to '0.0.0.0'.
-- `SERVER_PORT`: Specifies the port on which the Flask app will run. Defaults to 5000.
+- FLASK_DEBUG
+- SERVER_HOST
+- SERVER_PORT
 
 Usage:
-- Run this script directly to start the Flask application.
-
-Example:
     $ python run.py
 """
 
 import os
-
 from dotenv import load_dotenv
-
-from fuelrod_exporter.app import get_app as create_app
-from fuelrod_exporter.core.auto_reload import watch_env
 
 load_dotenv()
 
+# Determine debug mode early
+debug = os.getenv("FLASK_DEBUG") == "1"
+
+# Conditionally import watch_env only in development
+from fuelrod_exporter.core.auto_reload import watch_env
+
+from fuelrod_exporter.app import get_app as create_app
+
 app = create_app()
-app.app_context().push()  # Push the application context for use in the script
+app.app_context().push()
 
-
-if __name__ == '__main__':
-    # Use environment variables
-    debug = os.getenv('FLASK_DEBUG') == '1'
-    host = os.getenv('SERVER_HOST', default='0.0.0.0')
-    port = os.getenv('SERVER_PORT', default=3000)
+if __name__ == "__main__":
+    host = os.getenv("SERVER_HOST", default="0.0.0.0")
+    port = int(os.getenv("SERVER_PORT", default=3000))
     app.run(host=host, port=port, debug=debug)
